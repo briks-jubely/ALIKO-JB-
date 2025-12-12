418c9ada11ec5daf1d137d4cb07bd7646d4680a5
briks-jubely <jubelybriks@gmail.com>
2025-12-12 11:44:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,44 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'aliko-jb-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Workshop.html',
+  '/academy.html',
+  '/icon-192.png',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("alikojb-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(ALIKO_JB).then((cache) => {
+      console.log('Caching files for offline use');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== ALIKO_JB) {
+            console.log('Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("aliko-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/logo.png"]);
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
