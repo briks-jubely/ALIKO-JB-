71ab369f60b8c2b355c8e6b20977b4c2ad66dd90
briks-jubely <jubelybriks@gmail.com>
2025-12-19 23:39:44 +0300

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,27 @@
+<script type="module">
+import { collection, getDocs } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+async function loadCourses() {
+  const courseContainer = document.getElementById("courses");
+  courseContainer.innerHTML = "";
+
+  const querySnapshot = await getDocs(collection(db, "courses"));
+
+  querySnapshot.forEach((doc) => {
+    const c = doc.data();
+
+    courseContainer.innerHTML += `
+      <div class="card">
+        <img src="${c.image}">
+        <span class="badge ${c.status}">${c.status.toUpperCase()}</span>
+        <h3>${c.title}</h3>
+        <p>${c.description}</p>
+        <small>Level: ${c.level}</small>
+      </div>
+    `;
+  });
+}
+
+loadCourses();
+</script>
